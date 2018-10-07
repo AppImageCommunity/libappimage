974d9ecc64a116e03e83e3a4b51d8b8571808253
TheAssassin <theassassin@assassinate-you.net>
2018-10-07 21:23:27 +0200

    Move MD5 calculation to libappimage_shared
    CC https://github.com/AppImage/AppImageKit/issues/862

--- include/appimage/appimage_shared.h
+++ include/appimage/appimage_shared.h
@@ -21,6 +21,23 @@ int appimage_print_binary(char* fname, unsigned long offset, unsigned long lengt
  */
 char* appimage_hexlify(const char* bytes, size_t numBytes);
 
+/*
+ * Calculate MD5 digest of AppImage file, skipping the signature and digest sections.
+ *
+ * The digest section must be skipped as the value calculated by this method is going to be embedded in it by default.
+ *
+ * The signature section must be skipped as the signature will not be available at the time this hash is calculated.
+ *
+ * The hash is _not_ compatible with tools like md5sum.
+ *
+ * You need to allocate a char array of at least 16 bytes (128 bit) and pass a reference to it as digest parameter.
+ * The function will set it to the raw digest, without any kind of termination. Please use appimage_hexlify() if you
+ * need a textual representation.
+ *
+ * Please beware that this calculation is only available for type 2 AppImages.
+ */
+bool appimage_type2_digest_md5(const char* fname, char* digest);
+
 #ifdef __cplusplus
 }
 #endif
