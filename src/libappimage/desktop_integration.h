7f390f6d0055f822e39153a02a5ffd1472785004
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-10-24 21:19:32 +0200

    Remake of the desktop integration functions.
    Create an alternative implementation using the travese appimage algorithm to abstract the desktop integration logic from the AppImage management.
    Functions included:
     - extract desktop integration relevant files (.desktop, icons)
     - modify desktop file to be deployed.
     - deploy modified desktop file
     - deploy icons

---
+++ src/libappimage/desktop_integration.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Functions related to the desktop integration of AppImages.
+ */
+
+/**
+ * Create a temporary dir to extract the AppImage Desktop Integration relate files such as:
+ * - .Desktop file
+ * - .AppIcon
+ * - usr/share/icons/...
+ *
+ * @param appimage_path
+ * @return path to the temporary dir
+ */
+char* desktop_integration_create_tempdir();
+
+/**
+ * Extract the AppImage Desktop Integration relate files such as:
+ * - .Desktop file
+ * - .AppIcon
+ * - usr/share/icons/...
+ *
+ * @param appimage_path
+ * @param tempdir_path
+ */
+void desktop_integration_extract_relevant_files(const char* appimage_path, const char* tempdir_path);
+
+
+/**
+ * Modifies the fields on the .Desktop file to make them point to the expected locations of the desktop integration
+ * files.
+ * Fields modified:
+ * - Exec: will point to the AppImage
+ * - TryExcec: will point to the AppImage
+ * - Icon: Will point to the expected path of the application icon in $XDG_DATA_HOME/icons/.../apps/..
+ * @param appimage_path
+ * @param tempdir_path
+ */
+bool desktop_integration_modify_desktop_file(const char* appimage_path, const char* tempdir_path);
+
+
+/**
+ * Move .desktop file and application icons from <tempdir_path>/share to the $XDG_DATA_HOME
+ * @param tempdir_path
+ * @param user_data_dir
+ * @param md5sum
+ */
+bool desktop_integration_move_files_to_user_data_dir(const char* tempdir_path, const char* user_data_dir,
+                                                     const char* md5sum);
+
+/**
+ * Remove recusively remaining files at the temporary dir and the dir itself
+ * @param dir_path
+ */
+void desktop_integration_remove_tempdir(const char* dir_path);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ tests/file_management_utils.hpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <glib.h>
+#include <glib/gstdio.h>
+
+void removeDirRecursively(const std::string& path) {
+    GDir* tempdir = NULL;
+    GError* error;
+    tempdir = g_dir_open(path.c_str(), 0, &error);
+    if (!tempdir) {
+        g_warning("%s\n", error->message);
+        g_error_free(error);
+        return;
+    }
+
+    const char* entry;
+    while ((entry = g_dir_read_name(tempdir)) != NULL) {
+        char* full_path = g_strjoin("/", path.c_str(), entry, NULL);
+        if (g_file_test(full_path, G_FILE_TEST_IS_DIR)) {
+            removeDirRecursively(full_path);
+        } else
+            g_remove(full_path);
+
+        free(full_path);
+    }
+
+    g_rmdir(path.c_str());
+    g_dir_close(tempdir);
+}
+
+std::string createTempDir(const std::string& base_name) {
+    std::string result;
+    GError* error = NULL;
+    std::string template_name = base_name + "-XXXXXX";
+    char* path = g_dir_make_tmp(template_name.c_str(), &error);
+    if (error) {
+        g_warning("%s", error->message);
+        g_error_free(error);
+    } else {
+        result = path;
+        free(path);
+    }
+
+    return result;
+}
+
+bool copy_file(const char* source, const char* target) {
+    char *target_dir = g_path_get_dirname(target);
+    int res = g_mkdir_with_parents(target_dir, S_IRWXU);
+    g_free(target_dir);
+
+    if (res == -1)
+        return false;
+
+    int fin = open(source, O_RDONLY);
+    int fout = open(target, O_WRONLY | O_CREAT, S_IRWXU);
+
+    if (fin == -1 || fout == -1)
+        return  false;
+
+    const size_t BUF_SIZE = 1024;
+    char buf[BUF_SIZE];
+    size_t bs_read;
+    while ((bs_read = static_cast<size_t>(read(fin, &buf, BUF_SIZE))) != 0)
+        write(fout, &buf, bs_read);
+
+    close(fin);
+    close(fout);
+
+    return true;
+}
