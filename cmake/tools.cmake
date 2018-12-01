36a71a915737617c5d0e4d00c173d444b7bfe5e8
TheAssassin <theassassin@assassinate-you.net>
2018-12-01 01:06:42 +0100

    Force tools prefix on some programs, if set
    That will make sure that non-compatible version will be used
    implicitly.

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -1,49 +1,61 @@
 if(TOOLS_PREFIX)
-    message(STATUS "TOOLS_PREFIX ${TOOLS_PREFIX} detected")
+    message(STATUS "TOOLS_PREFIX set, looking for programs with prefix ${TOOLS_PREFIX}")
 endif()
 
 # first of all, make sure required programs are available
-function(check_program name)
+function(check_program)
+    set(keywords FORCE_PREFIX)
+    set(oneValueArgs NAME)
+    cmake_parse_arguments(ARG "${keywords}" "${oneValueArgs}" "" "${ARGN}")
+
+    if(NOT ARG_NAME)
+        message(FATAL_ERROR "NAME argument required for check_program")
+    endif()
+    
     if(TOOLS_PREFIX)
-        set(prefix ${TOOLS_PREFIX}_)
+        set(prefix ${TOOLS_PREFIX})
     endif()
 
-    message(STATUS "Checking for program ${name}")
+    message(STATUS "Checking for program ${ARG_NAME}")
 
-    string(TOUPPER ${name} name_upper)
+    string(TOUPPER ${ARG_NAME} name_upper)
 
     if(prefix)
         # try prefixed version first
-        find_program(${name_upper} ${prefix}${name})
+        find_program(${name_upper} ${prefix}${ARG_NAME})
     endif()
 
     # try non-prefixed version
     if(NOT ${name_upper})
-        find_program(${name_upper} ${name})
+        if(ARG_FORCE_PREFIX)
+            message(FATAL_ERROR "TOOLS_PREFIX set, but could not find program with prefix in PATH (FORCE_PREFIX is set)")
+        endif()
+
+        find_program(${name_upper} ${ARG_NAME})
 
         if(NOT ${name_upper})
-            message(FATAL_ERROR "Could not find required program ${name}.")
+            message(FATAL_ERROR "Could not find required program ${ARG_NAME}.")
         endif()
     endif()
 
-    message(STATUS "Found program ${name}: ${${name_upper}}")
+    message(STATUS "Found program ${ARG_NAME}: ${${name_upper}}")
 
     mark_as_advanced(${name_upper})
 endfunction()
 
-check_program(aclocal)
-check_program(autoheader)
-check_program(automake)
-check_program(autoreconf)
-check_program(libtoolize)
-check_program(patch)
-check_program(sed)
-check_program(wget)
-check_program(xxd)
-check_program(desktop-file-validate)
-check_program(objcopy)
-check_program(objdump)
-check_program(readelf)
-check_program(strip)
-check_program(make)
+check_program(NAME aclocal)
+check_program(NAME autoheader)
+check_program(NAME automake)
+check_program(NAME autoreconf)
+check_program(NAME libtoolize)
+check_program(NAME patch)
+check_program(NAME sed)
+check_program(NAME wget)
+check_program(NAME xxd)
+check_program(NAME desktop-file-validate)
+check_program(NAME objcopy FORCE_PREFIX)
+check_program(NAME objdump FORCE_PREFIX)
+check_program(NAME readelf FORCE_PREFIX)
+check_program(NAME strip FORCE_PREFIX)
+check_program(NAME make)
 # TODO: add checks for remaining commands
