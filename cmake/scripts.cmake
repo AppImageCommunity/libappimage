91065535b81be64b94d03cfa8ba7c34a934a0562
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-12-17 21:37:03 -0600

    Move configure_libappimage_module function to the scripts.cmake file

--- cmake/scripts.cmake
+++ cmake/scripts.cmake
@@ -219,3 +219,31 @@ function(import_external_project)
     set(${IMPORT_EXTERNAL_PROJECT_TARGET_NAME}_LIBRARY_DIRS "${IMPORT_EXTERNAL_PROJECT_LIBRARY_DIRS}" CACHE INTERNAL "")
     set(${IMPORT_EXTERNAL_PROJECT_TARGET_NAME}_PREFIX ${INSTALL_DIR} CACHE INTERNAL "")
 endfunction()
+
+# @brief Configure a libappimage module by setting
+#
+# Sets set to the given <target> the public headers, the compile definitions and the include directories. Which are
+# common to all modules.
+function(configure_libappimage_module target)
+    # targets are called lib* already, so CMake shouldn't add another lib prefix to the actual files
+    set_target_properties(${target}
+        PROPERTIES PREFIX ""
+        PUBLIC_HEADER ${libappimage_public_header}
+        POSITION_INDEPENDENT_CODE ON
+    )
+
+    target_compile_definitions(${target}
+        # Support Large Files
+        PRIVATE -D_FILE_OFFSET_BITS=64
+        PRIVATE -D_LARGEFILE_SOURCE
+
+        PRIVATE -DGIT_COMMIT="${GIT_COMMIT}"
+        PRIVATE -DENABLE_BINRELOC
+    )
+
+    target_include_directories(${target}
+        PUBLIC $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include/>
+        PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/src/libappimage>
+        INTERFACE $<INSTALL_INTERFACE:include/>
+    )
+endfunction()
