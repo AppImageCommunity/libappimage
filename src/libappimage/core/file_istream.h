d1b273531ac2feb377daad3b2103ac8ecf8e086b
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-12-18 22:08:04 -0600

    Add class purpose description

--- src/libappimage/core/file_istream.h
+++ src/libappimage/core/file_istream.h
@@ -5,6 +5,15 @@
 
 namespace appimage {
     namespace core {
+        /**
+         * @brief Convenience wrapper around std::streambuf to allow the creation of std::istream instances from the files
+         * contained inside a given AppImage.
+         *
+         * This class should be instanced by the AppImage traversal derivatives by providing the right std::streambuf
+         * derivative for a given AppImage format.
+         *
+         * @related traversal.h
+         */
         class file_istream : public std::istream {
             std::shared_ptr<std::streambuf> streambuf;
         public:
