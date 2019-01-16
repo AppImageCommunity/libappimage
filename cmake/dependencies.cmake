e289727a7c7422ba966266371a59da8d22a61054
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-01-16 12:45:00 -0600

    Try to fix cross compilation issue 3

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -143,11 +143,15 @@ endif()
 if(NOT USE_SYSTEM_BOOST)
     message(STATUS "Downloading and building boost")
 
-    ExternalProject_Add(boost-EXTERNAL
+    if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "i386")
+        set(BOOST_B2_TARGET_CONFIG "architecture=x86 address-model=32")
+    endif()
+
+    externalproject_add(boost-EXTERNAL
         URL https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
         URL_HASH SHA256=9a2c2819310839ea373f42d69e733c339b4e9a19deab6bfec448281554aa4dbb
         CONFIGURE_COMMAND ./bootstrap.sh --with-libraries=filesystem,system,thread
-        BUILD_COMMAND CC=${CC} CXX=${CXX} CFLAGS=${CFLAGS} CPPFLAGS=${CPPFLAGS} LDFLAGS=${LDFLAGS} ./b2 cxxflags=-fPIC cflags=-fPIC link=static
+        BUILD_COMMAND ./b2 ${BOOST_B2_TARGET_CONFIG} cxxflags=-fPIC ${CPPFLAGS} cflags=-fPIC ${CFLAGS} link=static
         INSTALL_COMMAND ""
         BUILD_IN_SOURCE 1
     )
