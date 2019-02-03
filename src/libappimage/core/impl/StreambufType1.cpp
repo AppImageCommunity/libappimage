1f64de486a0b66a70e6b07ab9bc7e0ac642c7b5f
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-02-03 10:31:59 -0600

    Notify errors while reading a payload file

--- src/libappimage/core/impl/StreambufType1.cpp
+++ src/libappimage/core/impl/StreambufType1.cpp
@@ -1,3 +1,5 @@
+// local
+#include <appimage/core/exceptions.h>
 #include "StreambufType1.h"
 
 using namespace appimage::core::impl;
@@ -6,8 +8,12 @@ int StreambufType1::underflow() {
     // Read line from original source
     auto bytesRead = archive_read_data(a, buffer.data(), size);
 
-    // notify eof if nothing was read (real eof or error)
-    if (!bytesRead) return traits_type::eof();
+    // In case of error a value lower than 0 is returned
+    if (bytesRead < 0)
+        throw IOError(archive_error_string(a));
+
+    // notify eof if nothing
+    if (bytesRead == 0) return traits_type::eof();
 
     // Update streambuf read pointers see <setg> doc
     setg(buffer.data(), buffer.data(), buffer.data() + bytesRead);
