040d49ebb18315aebe9f6e7af4094a73acf31dc5
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-02-07 01:23:39 -0600

    Comment the DesktopIntegrationResources entity

--- src/libappimage/desktop_integration/integrator/DesktopIntegrationResources.h
+++ src/libappimage/desktop_integration/integrator/DesktopIntegrationResources.h
@@ -6,6 +6,10 @@
 
 namespace appimage {
     namespace desktop_integration {
+        /**
+         * This entity is meant to hold the raw data of the resources required in the desktop integration process.
+         * It should not be filled manually, instead use the ResourcesExtractor class.
+         */
         struct DesktopIntegrationResources {
             std::string desktopEntryPath;
             std::vector<char> desktopEntryData;
