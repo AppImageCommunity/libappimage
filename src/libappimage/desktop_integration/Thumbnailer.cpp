c479c3886733d7cfe85cd0a0e7fb1fbd41c95d89
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-02-07 23:32:21 -0600

    Add comments

--- src/libappimage/desktop_integration/Thumbnailer.cpp
+++ src/libappimage/desktop_integration/Thumbnailer.cpp
@@ -22,24 +22,32 @@ namespace appimage {
         Thumbnailer::Thumbnailer() : xdgCacheHome(XdgUtils::BaseDir::Home() + "/.cache") {}
 
         Thumbnailer::Thumbnailer(const std::string& xdgCacheHome) : xdgCacheHome(xdgCacheHome) {
+            /* XDG_CACHE_HOME path is required to deploy the thumbnails */
             if (Thumbnailer::xdgCacheHome.empty())
                 Thumbnailer::xdgCacheHome = XdgUtils::BaseDir::Home() + "/.cache";
         }
 
         void Thumbnailer::create(const core::AppImage& appImage) {
             auto resources = extractResources(appImage);
+            /* Just the application main icon will be used to generate the thumbnails */
             std::string appIcon = getAppIconName(resources);
 
+            /* According to the xdg thumbnails spec files should be named after the
+             * md5 sum of it's canonical path. */
             std::string canonicalPathMd5 = getCanonicalPathMd5(appImage.getPath());
 
+            /* Get from the resources the icon that will be easily resized to 128x128  */
             auto normalIconData = getIconData(resources, appIcon, "128x128");
             generateNormalSizeThumbnail(canonicalPathMd5, normalIconData);
 
+            /* Get from the resources the icon that will be easily resized to 256x256  */
             auto largeIconData = getIconData(resources, appIcon, "256x256");
             generateLargeSizeThumbnail(canonicalPathMd5, largeIconData);
         }
 
         void Thumbnailer::remove(const core::AppImage& appImage) {
+            /* Every resource file related with this appimage has the md5 sum of the appimage canonical
+             * path in its name, we are going to use this to recreate the file names */
             std::string canonicalPathMd5 = getCanonicalPathMd5(appImage.getPath());
             bf::path normalThumbnailPath = getNormalThumbnailPath(canonicalPathMd5);
             bf::path largeThumbnailPath = getLargeThumbnailPath(canonicalPathMd5);
@@ -52,15 +60,23 @@ namespace appimage {
                                                       std::vector<char>& normalIconData) const {
 
             bf::path normalThumbnailPath = getNormalThumbnailPath(canonicalPathMd5);
+
+            /* It required that the folders were the thumbnails will be deployed to exists */
             bf::create_directories(normalThumbnailPath.parent_path());
 
             try {
                 std::vector<uint8_t> iconData{normalIconData.begin(), normalIconData.end()};
                 IconHandle iconHandle(iconData);
+                /* large thumbnails are 128x128, let's be sure of it*/
                 iconHandle.setSize(128);
                 iconHandle.save(normalThumbnailPath.string(), "png");
                 return;
-            } catch (const IconHandleError&) {}
+            } catch (const IconHandleError&) {
+                /* we fail to resize the icon because it's in an unknown format or some other reason
+                 * we just have left to write it down as it is and hope for the best. */
+                std::clog << "Unable to resize the application icon into a 128x128 image, it will be "
+                             "written as it's.";
+            }
 
             // It wasn't possible to generate a thumbnail, therefore the the icon will be written as it's
             bf::ofstream out(normalThumbnailPath);
@@ -70,15 +86,25 @@ namespace appimage {
         void Thumbnailer::generateLargeSizeThumbnail(const std::string& canonicalPathMd5,
                                                      std::vector<char>& largeIconData) const {
             bf::path largeThumbnailPath = getLargeThumbnailPath(canonicalPathMd5);
+
+            /* It required that the folders were the thumbnails will be deployed to exists */
             bf::create_directories(largeThumbnailPath.parent_path());
 
             try {
                 std::vector<uint8_t> iconData{largeIconData.begin(), largeIconData.end()};
                 IconHandle iconHandle(iconData);
+                /* large thumbnails are 256x256, let's be sure of it*/
                 iconHandle.setSize(256);
+
+                /* thumbnails are always png */
                 iconHandle.save(largeThumbnailPath.string(), "png");
                 return;
-            } catch (const IconHandleError&) {}
+            } catch (const IconHandleError&) {
+                /* we fail to resize the icon because it's in an unknown format or some other reason
+                 * we just have left to write it down as it is and hope for the best. */
+                std::clog << "Unable to resize the application icon into a 256x256 image, it will be "
+                             "written as it's.";
+            }
 
             // It wasn't possible to generate a thumbnail, therefore the the icon will be written as it's
             bf::ofstream out(largeThumbnailPath);
@@ -116,6 +142,7 @@ namespace appimage {
                                                    const std::string& appIcon, const std::string& iconSize) {
             std::vector<char> iconData;
 
+            /* look for a perfect match */
             for (const auto& itr: resources.icons) {
                 if (itr.first.find(appIcon) != std::string::npos) {
                     if (itr.first.find(iconSize) != std::string::npos)
@@ -123,6 +150,7 @@ namespace appimage {
                 }
             }
 
+            /* .DirIcon if there are no perfect matches. Provably we will have to resize it latter */
             auto dirIconItr = resources.icons.find(".DirIcon");
             if (iconData.empty() && dirIconItr != resources.icons.end())
                 iconData = dirIconItr->second;
@@ -143,6 +171,7 @@ namespace appimage {
         }
 
         DesktopIntegrationResources Thumbnailer::extractResources(const core::AppImage& appImage) const {
+            /* We only need the Desktop Entry and the icons */
             integrator::ResourcesExtractor extractor(appImage);
             extractor.setExtractAppDataFile(false);
             extractor.setExtractMimeFiles(false);
