2158b12ade4854a5088e83cccc4c5a8923332ad1
TheAssassin <theassassin@assassinate-you.net>
2020-12-10 19:01:31 +0100

    Switch to GitHub actions

--- ci/build-and-test.sh
+++ ci/build-and-test.sh
@@ -10,7 +10,7 @@ else
     TEMP_BASE=/tmp
 fi
 
-BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" AppImageUpdate-build-XXXXXX)
+BUILD_DIR="$(mktemp -d -p "$TEMP_BASE" AppImageUpdate-build-XXXXXX)"
 
 cleanup () {
     if [ -d "$BUILD_DIR" ]; then
@@ -21,31 +21,31 @@ cleanup () {
 trap cleanup EXIT
 
 # store repo root as variable
-REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
-OLD_CWD=$(readlink -f .)
+REPO_ROOT="$(readlink -f "$(dirname "$(dirname "$0")")")"
+OLD_CWD="$(readlink -f .)"
 
 pushd "$BUILD_DIR"
 
 # configure build
 if [ "$ENABLE_COVERAGE" == "On" ]; then
     cmake "$REPO_ROOT" -DCMAKE_INSTALL_LIBDIR=lib -DENABLE_COVERAGE=On
-    make -j$(nproc) coverage
+    make -j"$(nproc)" coverage
 else
     cmake "$REPO_ROOT" -DCMAKE_INSTALL_LIBDIR=lib
 
     # build binaries
-    make -j$(nproc)
+    make -j"$(nproc)"
 
     # run all unit tests
     ctest -V
 fi
 
 # install libappimage
-DESTDIR=$BUILD_DIR/libappimage make install
+DESTDIR="$BUILD_DIR"/libappimage make install
 
 # do integration test
-mkdir $BUILD_DIR/client_app_build
-pushd $BUILD_DIR/client_app_build
-cmake -DCMAKE_PREFIX_PATH=$BUILD_DIR/libappimage/usr/local/lib/cmake/libappimage $REPO_ROOT/tests/client_app/
+mkdir "$BUILD_DIR"/client_app_build
+pushd "$BUILD_DIR"/client_app_build
+cmake -DCMAKE_PREFIX_PATH="$BUILD_DIR"/libappimage/usr/local/lib/cmake/libappimage "$REPO_ROOT"/tests/client_app/
 make
 ./client_app
--- ci/ci-build.sh
+++ ci/ci-build.sh
@@ -3,17 +3,22 @@
 set -e
 set -x
 
+if [ "$ARCH" == "" ]; then
+    echo "Usage: env ARCH=... [DOCKER_IMAGE=...] bash $0"
+    exit 2
+fi
+
 # build libappimage, and run unit tests
 if [ "$DOCKER_IMAGE" != "" ]; then
     docker run --rm \
         --cap-add SYS_ADMIN \
         --device /dev/fuse:mrw \
-        -e ARCH -e TRAVIS -e TRAVIS_BUILD_NUMBER \
+        -e ARCH -e CI=1 \
         -e CI=1 \
         -i \
         -v "${PWD}":/libappimage \
         "$DOCKER_IMAGE" \
-        /bin/bash -xc "cd /libappimage && travis/build-and-test.sh"
+        /bin/bash -xc "cd /libappimage && ci/build-and-test.sh"
 else
-    exec $(readlink -f $(dirname "$0"))/build-and-test.sh
+    exec "$(readlink -f "$(dirname "$0")")"/build-and-test.sh
 fi
