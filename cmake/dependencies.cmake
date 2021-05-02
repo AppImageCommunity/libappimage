7ea66f67f60c5f6d613ea0a995042cb3daf1a145
TheAssassin <theassassin@assassinate-you.net>
2021-05-03 01:43:15 +0200

    Fix Boost download URL

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -161,7 +161,7 @@ if(NOT USE_SYSTEM_BOOST)
 
     ExternalProject_Add(
         boost-EXTERNAL
-        URL https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+        URL https://downloads.sourceforge.net/project/boost/boost/1.69.0/boost_1_69_0.tar.gz
         URL_HASH SHA256=9a2c2819310839ea373f42d69e733c339b4e9a19deab6bfec448281554aa4dbb
         CONFIGURE_COMMAND ./bootstrap.sh --with-libraries=filesystem,system,thread --with-toolset=${boost_compiler_id}
         BUILD_COMMAND ./b2 ${BOOST_B2_TARGET_CONFIG} cxxflags=-fPIC ${CPPFLAGS} cflags=-fPIC ${CFLAGS} link=static
